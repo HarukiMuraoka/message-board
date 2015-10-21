7fa445cdcdbb7e08c835a0e8010f41d42b3b1a34
harukimuraoka <ironman42876@ezweb.ne.jp>
2015-10-21 14:22:41 +0000

    Implementation of the edit

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,48 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
   def index
-    @messages=Message.all
+    @messages = Message.all
+    @message = Message.new
   end
-  
   
   def create
-    @message=Message.new(message_params)
-    @message.save
-    redirect_to root_path , notice: 'メッセージを保存しました'
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to root_path , notice: 'メッセージを保存しました'
+    else
+      @messages = Message.all
+      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      render 'index'
+    end
   end
   
-  private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
+  
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  
+ 
+
 end
+
